961c51e10cc03cf9d004c263a7c067fec9839f1b
Rushidikkar3 <rushikeshdikkar2708@gmail.com>
2024-04-29 13:05:30 +0530

    first commit

---
+++ e-comerce-backend/backend/data/products.js
@@ -0,0 +1,58 @@
+const products = [
+  {
+    name: "Stainless Steel Cooking Pot",
+    imageUrl:
+      "https://images.unsplash.com/photo-1604414499020-f9ac575bc5ec?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description:
+      "This stainless steel cooking pot is perfect for all your cooking needs. With its durable construction and non-reactive surface, it ensures even heat distribution for consistent cooking results. Whether you're boiling pasta, making soup, or simmering sauces, this cooking pot is a kitchen essential.",
+    price: 25,
+    countInStock: 20,
+  },
+  {
+    name: "Stainless Steel Mixing Bowls Set",
+    imageUrl:
+      "https://media.istockphoto.com/id/2111418470/photo/stainless-steel-bowls-in-various-sizes.jpg?s=1024x1024&w=is&k=20&c=-G6fFjEkK4IRfqUh4P4i5JwC6BFQOU5do8SR-4wxN9Q=",
+    description:
+      "Upgrade your kitchen with this set of stainless steel mixing bowls. These bowls are ideal for mixing ingredients, whisking sauces, and storing leftovers. The non-slip silicone bottoms ensure stability while mixing, and the nesting design allows for easy storage. With various sizes, you'll have the perfect bowl for every task.",
+    price: 30,
+    countInStock: 15,
+  },
+  {
+    name: "Stainless Steel Cutlery Set",
+    imageUrl:
+      "https://images.unsplash.com/photo-1607637508318-d22cfb4299d4?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description:
+      "Add elegance to your dining table with this stainless steel cutlery set. Crafted from high-quality stainless steel, this set includes forks, knives, and spoons, providing everything you need for a formal dinner or casual meal. The sleek design and mirror finish make these utensils a stylish addition to any table setting.",
+    price: 50,
+    countInStock: 10,
+  },
+  {
+    name: "Stainless Steel Saucepan with Lid",
+    imageUrl:
+      "https://media.istockphoto.com/id/480487572/photo/open-kitchen-pot-with-glass-lid-isolated-on-white.jpg?s=1024x1024&w=is&k=20&c=DGrMyoG9BLrA08yN0_eQLBtQynZBHitw2nicPl67XZk=",
+    description:
+      "Prepare delicious sauces, soups, and more with this stainless steel saucepan. The encapsulated base ensures even heat distribution, while the tempered glass lid allows you to monitor cooking progress without lifting it. Whether you're simmering, boiling, or reheating, this saucepan is a versatile tool for any kitchen.",
+    price: 35,
+    countInStock: 12,
+  },
+  {
+    name: "Stainless Steel Cooking Utensil Set",
+    imageUrl:
+      "https://media.istockphoto.com/id/1982124570/photo/stainless-steel-lota-image-with-selective-focus.jpg?s=1024x1024&w=is&k=20&c=TD9yORW8Kqt1hO0lJrxiMoNvuXq5KhTpow5rTaQVGVk=",
+    description:
+      "Upgrade your cooking experience with this set of stainless steel cooking utensils. From flipping pancakes to stirring soups, these utensils are designed for durability and functionality. The ergonomic handles provide a comfortable grip, while the hanging loops offer convenient storage. Equip your kitchen with these essential tools and elevate your culinary skills.",
+    price: 40,
+    countInStock: 8,
+  },
+  {
+    name: "Stainless Steel Roasting Pan with Rack",
+    imageUrl:
+      "https://media.istockphoto.com/id/1435646252/photo/electric-oven.jpg?s=1024x1024&w=is&k=20&c=lSn-UodURQtx8XVe8bV7wFh0iyfs4bD9T8tIQ_NJwsA=",
+    description:
+      "Roast meats and vegetables to perfection with this stainless steel roasting pan. The removable rack elevates food for even cooking and allows air to circulate for crispy results. Whether you're roasting a turkey for Thanksgiving or baking a tray of veggies for dinner, this roasting pan is a must-have for any home chef.",
+    price: 60,
+    countInStock: 5,
+  },
+];
+
+module.exports = products;
