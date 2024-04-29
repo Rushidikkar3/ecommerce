02cdd7a13d4d10d15d24b0366b89d5e5b64fe5d3
Rushidikkar3 <168056519+Rushidikkar3@users.noreply.github.com>
2024-04-29 23:08:27 +0530

    Update server.js

--- e-comerce-backend/backend/server.js
+++ e-comerce-backend/backend/server.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const path = require("path");
 const express = require("express");
 const productRoutes = require("./routes/productRoutes");
 const userRoutes = require("./routes/userRoutes");
@@ -10,16 +11,24 @@ connectDB();
 
 const app = express();
 
-app.use(express.json());
-app.use(cors());
+const baseDir = path.resolve();
+
+app.use(express.static(path.join(baseDir, "e-comerce-frontend/public/dist")));
 
 app.get("/", (req, res) => {
   res.json({ message: "API running..." });
 });
 
+app.use(express.json());
+app.use(cors());
+
 app.use("/api/products", productRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/cart", cartRoutes);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(baseDir, "e-comerce-frontend/public", "dist", "index.html"));
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
